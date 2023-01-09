0fdd4aad264c011f8d688bdee01b104fe5680076
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-01-09 11:26:53 +0700

    props

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <User />
-      <User />
-      <User />
+      <User name="Ghulam" umur="22" email="ghulam@ghulam.com" />
+      <User name="Ammar" umur="23" email="ammar@ammar.com" />
+      <User name="Yanuar" umur="24" email="yanuar@yanuar.com" />
     </div>
   );
 }
 
-const User = () => {
+const User = (props) => {
   return (
     <div>
-      <h1>Ghulam</h1>
-      <h1>22</h1>
-      <h1>ghulam@ghulam.com</h1>
+      <h1>{props.name}</h1>
+      <h1>{props.umur}</h1>
+      <h1>{props.email}</h1>
     </div>
   );
 };
