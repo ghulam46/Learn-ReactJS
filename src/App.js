f64532a1be664cbffdfe1c035ef27dd2f5aaa8c9
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-01-09 10:59:47 +0700

    5

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ function App() {
   const name = <h1>Ghulam</h1>;
   const age = <h2>22</h2>;
   const email = <h2>ghulam.yanuar01@gmail.com</h2>;
+  const user = (
+    <div>
+      {name}
+      {age}
+      {email}
+    </div>
+  );
+
   return (
     <div className="App">
-      <div>
-        {name}
-        {age}
-        {email}
-      </div>
+      {user}
+      {user}
+      {user}
     </div>
   );
 }
