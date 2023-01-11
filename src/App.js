56c228f9af9dbff20e9b978c52011c3ffc65365e
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-01-11 09:48:47 +0700

    Ternary Operator in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import "./App.css";
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <User name="Ghulam" age="22" email="ghulam@ghulam.com" />
+//       <User name="Ammar" age="23" email="ammar@ammar.com" />
+//       <User name="Yanuar" age="24" email="yanuar@yanuar.com" />
+//     </div>
+//   );
+// }
+
+// const User = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h1>{props.age}</h1>
+//       <h1>{props.email}</h1>
+//     </div>
+//   );
+// };
+
 function App() {
+  const age = 19;
+
   return (
     <div className="App">
-      <User name="Ghulam" age="22" email="ghulam@ghulam.com" />
-      <User name="Ammar" age="23" email="ammar@ammar.com" />
-      <User name="Yanuar" age="24" email="yanuar@yanuar.com" />
+      {/* Ternary Operator in React */}
+      {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
     </div>
   );
-}
 
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
-    </div>
-  );
-};
+  // Normal Operator with if else
+  // if (age >= 18) {
+  //   return <h1>OVER AGE</h1>;
+  // } else {
+  //   return <h1>UNDER AGE</h1>;
+  // };
+}
 
 export default App;
