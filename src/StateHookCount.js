efa25323db350ba7800887d2887bd73be22905b8
ghulam46 <ghulam.yanuar01@gmail.com>
2023-02-14 13:22:36 +0700

    useState Hook

---
+++ src/StateHookCount.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import {useState} from "react";
+
+function StateHookCount() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    );
+}
+
+export default StateHookCount;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ToDoList from "./ToDoList";
+import StateHookCount from "./StateHookCount";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <ToDoList />
+    <StateHookCount />
   </React.StrictMode>
 );
