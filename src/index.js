a340770f7ca7fa2f16942603e9515a62c2605fca
ghulam46 <ghulam.yanuar01@gmail.com>
2023-02-15 15:32:49 +0700

    useEffect

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import ToDoList from "./ToDoList";
 import StateHookCount from "./StateHookCount";
+import UseEffect from "./UseEffect/UseEffect";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <ToDoList />
-    <StateHookCount />
+    {/* <ToDoList /> */}
+    {/* <StateHookCount /> */}
+    <UseEffect />
   </React.StrictMode>
 );
