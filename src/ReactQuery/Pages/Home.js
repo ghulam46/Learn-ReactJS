a5b389f6d89005abe8b511ea8af1b5f9a445bfdb
ghulam46 <ghulam.yanuar01@gmail.com>
2023-04-13 10:03:12 +0700

    isLoading feature

--- src/ReactQuery/Pages/Home.js
+++ src/ReactQuery/Pages/Home.js
@@ -2,9 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const Home = () => {
-  const { data } = useQuery(["cat"], () => {
+  const { data, isLoading } = useQuery(["cat"], () => {
     return axios.get("https://catfact.ninja/fact").then((res) => res.data);
   });
+
+  // loading feature
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <h1>
